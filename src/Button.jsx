9a16585faaf964b9245cd866f2f5894bf431f4d8
Cong Nguyen <congthanh3806@gmail.com>
2025-09-30 18:39:57 -0700

    test

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,5 +1,5 @@
 export default function Button() {
-  return <button style={{borderRadius: '4px'}}>
+  return <button style={{borderRadius: '4px', border: '1px solid red'}}>
     Hello World
   </button>
 }
